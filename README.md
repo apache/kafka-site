0052cef69149fde4f3fe11db53cebb5622de8bae
QingWei <l.q.wei91@gmail.com>
2022-05-13 13:58:52 +0100

    Run the container in foreground mode

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ You can run it with the following command, note that it requires docker:
 ```
 
 Then you can open [localhost:8080](http://localhost:8080) on your browser and browse the documentation.
+
+To kill the process, just type ctrl + c
--- start-preview.sh
+++ start-preview.sh
@@ -4,6 +4,4 @@ set -euxo pipefail
 
 docker build -t kafka-site-preview .
 
-docker run -dit --rm --name mypreview -p 8080:80 -v "$PWD":/usr/local/apache2/htdocs/ kafka-site-preview
-
-echo "You can stop the preview server by running `docker stop mypreview`"
+docker run -it --rm --name mypreview -p 8080:80 -v "$PWD":/usr/local/apache2/htdocs/ kafka-site-preview
