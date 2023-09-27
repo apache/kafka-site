529f78f79239f4aea4bcfbca54697fce44a347ab
QingWei <l.q.wei91@gmail.com>
2023-09-27 19:14:46 +0100

    Address comment to make it work for podman

--- start-preview.sh
+++ start-preview.sh
@@ -4,4 +4,4 @@ set -euxo pipefail
 
 docker build -t kafka-site-preview .
 
-docker run -it --rm --name mypreview -p 8080:80 -v "$PWD":/usr/local/apache2/htdocs/ kafka-site-preview
+docker run -it --rm --name mypreview -p 8080:80 -v "$PWD":/usr/local/apache2/htdocs/:z kafka-site-preview
