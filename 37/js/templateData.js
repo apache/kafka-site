53e52c4d050ccd65a7a37a6170d3bae984964878
Matthias J. Sax <matthias@confluent.io>
2024-12-05 19:33:33 -0800

    MINOR: update docs for 3.7.2 release

--- 37/js/templateData.js
+++ 37/js/templateData.js
@@ -19,6 +19,6 @@ limitations under the License.
 var context={
     "version": "37",
     "dotVersion": "3.7",
-    "fullDotVersion": "3.7.1",
+    "fullDotVersion": "3.7.2",
     "scalaVersion": "2.13"
 };
