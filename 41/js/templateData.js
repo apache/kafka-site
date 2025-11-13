11fd18dfdc4559e443091d5bfa9fe7f6ee5e4afc
Lianet Magrans <98415067+lianetm@users.noreply.github.com>
2025-11-13 14:52:00 -0500

    MINOR: updates for 4.1.1 (#741)
    * updates for 4.1.1
    
    * address comment

--- 41/js/templateData.js
+++ 41/js/templateData.js
@@ -19,6 +19,6 @@ limitations under the License.
 var context={
     "version": "41",
     "dotVersion": "4.1",
-    "fullDotVersion": "4.1.0",
+    "fullDotVersion": "4.1.1",
     "scalaVersion": "2.13"
 };
